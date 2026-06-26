633155ab3715003075e74cdb614f75ec5be1335b
Aryan Pawar <aryanpawar049120@gmail.com>
2026-06-26 10:29:25 +0530

    fix: ensure required application static directories exist for production deployment build phases

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt --no-cache-dir --no-compile
 
+# Ensure necessary application static directories exist before running collectstatic
+mkdir -p custom_admin/static
+mkdir -p static
+mkdir -p staticfiles
+
 # Gather static layout configurations for Whitenoise
 python manage.py collectstatic --no-input
 
